5292c28c0712dc6204d9f1741bf3e52f939686d4
Tom Bereknyei <tomberek@gmail.com>
2022-06-16 13:31:03 -0400

    fix: revert to normal lib

--- flake.nix
+++ flake.nix
@@ -1,46 +1,49 @@
-{
-  inputs.capacitor.url = "git+ssh://git@github.com/flox/minicapacitor?ref=main&dir=capacitor&rev=2daa3287db735981dc91186232b5446b4f93e9f3";
+rec {
+  description = "Template using specific versions";
+
+  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor";
   inputs.capacitor.inputs.root.follows = "/";
 
-  inputs.nixpkgs-stable.url = "github:flox/nixpkgs/stable";
-  inputs.nixpkgs-unstable.url = "github:flox/nixpkgs/unstable";
-  inputs.nixpkgs-staging.url = "github:flox/nixpkgs/staging";
-  inputs.flox-c-env.url = "path:./templates/flox-c-env";
-  inputs.pkgs.url = "path:./pkgs";
-  inputs.pkgs.inputs.capacitor.follows = "capacitor";
+  inputs.nixpkgs.url = "git+ssh://git@github.com/flox/nixpkgs-flox";
+  inputs.nixpkgs.inputs.capacitor.follows = "capacitor";
 
-  nixConfig.bash-prompt = "[flox]\\e\[38;5;172mλ \\e\[m";
+  inputs.floxpkgs.url = "git+ssh://git@github.com/flox/floxpkgs";
+  inputs.floxpkgs.inputs.capacitor.follows = "capacitor";
+  inputs.floxpkgs.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.floxpkgs.inputs.ops-env.follows = "/";
 
-  outputs = {capacitor, ...} @ args:
-    capacitor args (
-      {
-        lib,
-        auto,
-        has,
-        ...
-      }:
-        has.stabilities
-        rec {
-          stable = args.nixpkgs-stable;
-          unstable = args.nixpkgs-unstable;
-          staging = args.nixpkgs-staging;
-          default = stable;
-        }
-        has.systems ["x86_64-linux"]
+  # TODO: preferred method below would only need this. (https://github.com/NixOS/nix/issues/5790)
+  # inputs.floxpkgs.url = "git+ssh://git@github.com/flox/floxpkgs";
+  # inputs.floxpkgs.inputs.capacitor.inputs.root.follows = "/";
+
+  nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
+
+  outputs = _:
+    _.capacitor _ ({floxpkgs,auto,lib, ...}: {
+      devShells.default = _.capacitor.lib.mkFloxShell _ ./flox.toml _.self.__pins;
+      apps = floxpkgs.apps;
+      legacyPackages = { pkgs,  ...  }: auto.automaticPkgsWith inputs ./pkgs pkgs;
+
+      # AUTO-MANAGED AFTER THIS POINT ##################################
+      # AUTO-MANAGED AFTER THIS POINT ##################################
+      # AUTO-MANAGED AFTER THIS POINT ##################################
+      __pins.versions = [
+        (builtins.getFlake "github:NixOS/nixpkgs/43cc623340ac0723fb73c1bce244bb6d791c5bb9").legacyPackages.x86_64-linux.curl
+        (builtins.getFlake "github:NixOS/nixpkgs/4d922730369d1c468cc4ef5b2fc30fd4200530e0").legacyPackages.x86_64-linux.kubernetes-helm
+      ];
+      __pins.vscode-extensions = [
         {
-          __reflect = {
-            projects = {
-              pkgs = auto.callSubflake "pkgs" {};
-            };
-          };
-
-          templates =
-            builtins.mapAttrs
-            (k: v: {
-              path = v.path;
-              description = (import (v.path + "/flake.nix")).description or "no description provided in ${v.path}/flake.nix";
-            })
-            (capacitor.lib.dirToAttrs ./templates {});
+          name = "python";
+          publisher = "ms-python";
+          sha256 = "0dxlqyhcfmb0bqbny633g0hwcq5ac7nz5rrq1c795bs3pimm8p20";
+          version = "2022.7.11541009";
         }
-    );
+        {
+          name = "pylint";
+          publisher = "ms-python";
+          sha256 = "0zca7w55v5xw69d7fva0mq5swc9ax36v598khzvrq3p4nzfjvfhk";
+          version = "2022.1.11541003";
+        }
+      ];
+    });
 }
--- pkgs/hello-upstream/default.nix
+++ pkgs/hello-upstream/default.nix
@@ -1,13 +1,13 @@
 # Copied as an example from: https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/applications/misc/hello/default.nix
-{
-  callPackage,
-  lib,
-  stdenv,
-  fetchurl,
-  nixos,
-  testers,
-  hello,
+{ callPackage
+, lib
+, stdenv
+, fetchurl
+, nixos
+, testers
+, hello
 }:
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "hello";
   version = "2.12";
@@ -20,16 +20,16 @@ stdenv.mkDerivation (finalAttrs: {
   doCheck = true;
 
   passthru.tests = {
-    version = testers.testVersion {package = hello;};
+    version = testers.testVersion { package = hello; };
 
     invariant-under-noXlibs =
       testers.testEqualDerivation
-      "hello must not be rebuilt when environment.noXlibs is set."
-      hello
-      (nixos {environment.noXlibs = true;}).pkgs.hello;
+        "hello must not be rebuilt when environment.noXlibs is set."
+        hello
+        (nixos { environment.noXlibs = true; }).pkgs.hello;
   };
 
-  passthru.tests.run = callPackage ./test.nix {hello = finalAttrs.finalPackage;};
+  passthru.tests.run = callPackage ./test.nix { hello = finalAttrs.finalPackage; };
 
   meta = with lib; {
     description = "A program that produces a familiar, friendly greeting";
@@ -40,7 +40,7 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://www.gnu.org/software/hello/manual/";
     changelog = "https://git.savannah.gnu.org/cgit/hello.git/plain/NEWS?h=v${finalAttrs.version}";
     license = licenses.gpl3Plus;
-    maintainers = [maintainers.eelco];
+    maintainers = [ maintainers.eelco ];
     platforms = platforms.all;
   };
 })
