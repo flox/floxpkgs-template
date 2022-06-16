d39beff57b0e7f223ae0a31b7ebcc7d39b08608c
Tom Bereknyei <tomberek@gmail.com>
2022-06-16 13:34:31 -0400

    fix: rename input floxpkgs to flox

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@ rec {
   inputs.nixpkgs.url = "git+ssh://git@github.com/flox/nixpkgs-flox";
   inputs.nixpkgs.inputs.capacitor.follows = "capacitor";
 
-  inputs.floxpkgs.url = "git+ssh://git@github.com/flox/floxpkgs";
-  inputs.floxpkgs.inputs.capacitor.follows = "capacitor";
-  inputs.floxpkgs.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.floxpkgs.inputs.ops-env.follows = "/";
+  inputs.flox.url = "git+ssh://git@github.com/flox/floxpkgs";
+  inputs.flox.inputs.capacitor.follows = "capacitor";
+  inputs.flox.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flox.inputs.ops-env.follows = "/";
 
   # TODO: preferred method below would only need this. (https://github.com/NixOS/nix/issues/5790)
   # inputs.floxpkgs.url = "git+ssh://git@github.com/flox/floxpkgs";
@@ -19,9 +19,9 @@ rec {
   nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
 
   outputs = _:
-    _.capacitor _ ({floxpkgs,auto,lib, ...}: {
+    _.capacitor _ ({flox,auto,lib, ...}: {
       devShells.default = _.capacitor.lib.mkFloxShell _ ./flox.toml _.self.__pins;
-      apps = floxpkgs.apps;
+      apps = flox.apps;
       legacyPackages = { pkgs,  ...  }: auto.automaticPkgsWith inputs ./pkgs pkgs;
 
       # AUTO-MANAGED AFTER THIS POINT ##################################
